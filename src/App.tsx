0f2d3c3d10bb4c3584652d0f58dbb6ae4c8cf2bc
Tekhronkhudaykulov <tekhronkhudaykulov@gmail.com>
2024-09-23 17:16:29 +0500

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import "./App.css";
 import Router from "./router/Router";
 import { ConfigProvider } from "antd";
-import { I18nextProvider } from "react-i18next";
-import i18n from "../i18n";
+
 import "./assets/fonts/fonts.css";
 import "react-simple-keyboard/build/css/index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -21,13 +20,11 @@ const queryClient = new QueryClient({
 
 const App = () => {
   return (
-    <I18nextProvider i18n={i18n}>
-      <QueryClientProvider client={queryClient}>
-        <ConfigProvider>
-          <Router />
-        </ConfigProvider>
-      </QueryClientProvider>
-    </I18nextProvider>
+    <QueryClientProvider client={queryClient}>
+      <ConfigProvider>
+        <Router />
+      </ConfigProvider>
+    </QueryClientProvider>
   );
 };
 
